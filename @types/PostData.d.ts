c3e24b57a4dbd9a5de428530af07e6ebec3144f7
kadoshita <sublimer.me@gmail.com>
2023-11-24 13:48:20 +0900

    Add comment field

--- @types/PostData.d.ts
+++ @types/PostData.d.ts
@@ -3,9 +3,10 @@ export interface PostData {
     url: string;
     category: string;
     description?: string;
+    comment?: string;
     image?: string;
     tag?: string[];
-};
+}
 
 export interface InsertPostData extends PostData {
     added_at: Date;
