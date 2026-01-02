d98e4344e8b1fc543807c277b88c67b44f381c00
kadoshita <sublimer.me@gmail.com>
2026-01-02 10:08:55 +0900

    Remove env var checks for MONGODB_URI and MONGODB_DB to avoid undefined error when build

--- db/index.ts
+++ db/index.ts
@@ -2,14 +2,6 @@ import { type Db, MongoClient, type MongoClientOptions } from 'mongodb';
 
 const { MONGODB_URI, MONGODB_DB } = process.env;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
-if (!MONGODB_DB) {
-  throw new Error('Please define the MONGODB_DB environment variable inside .env.local');
-}
-
 interface mongo {
   conn: { client: MongoClient; db: Db } | null;
   promise: Promise<{ client: MongoClient; db: Db }> | null;
